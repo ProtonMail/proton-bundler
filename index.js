c7ee37ede4b7d7d369c389c750e27e5269be0b05
dhoko <dhoko@pm.me>
2019-10-16 09:49:32 +0200

    Remove old code to send a message

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const {
     customBundler: { tasks: customTasks, config: customConfig },
     getCustomHooks
 } = require('./lib/custom');
-const { pull, push, getConfig, logCommits, generateChangelog } = require('./lib/git');
+const { pull, push, getConfig, generateChangelog } = require('./lib/git');
 const askDeploy = require('./lib/ask');
 const buildRemote = require('./lib/buildRemote');
 
@@ -267,12 +267,7 @@ async function main() {
     isCI && success(`Build CI app to the directory: ${chalk.bold('dist')}`, { time });
 
     if (!isCI && !argv.silentMessage) {
-        return logCommits(branch, flowType).then((data) => {
-            if (/deploy-(beta|prod|old|tor|dev)/.test(branch)) {
-                const [, env] = branch.match(/deploy-(beta|prod|old|tor|dev)/);
-                coucou.send(data, { env, flowType }, PKG);
-            }
-        });
+        return askDeploy(branch, PKG, argv);
     }
 }
 
@@ -281,31 +276,16 @@ if (argv._.includes('hosts')) {
 }
 
 if (argv._.includes('log-commits')) {
-    const parseEnv = (branch, website) => {
+    const parseEnv = ({ branch, website }) => {
         if (website && /deploy-(a|b)$/.test(branch)) {
             return 'deploy-prod';
         }
         return branch;
     };
 
-    const { branch, flow: flowType, custom } = argv;
     debug(argv, 'arguments');
-    const branchName = parseEnv(branch, argv.website);
-
+    const branchName = parseEnv(argv);
     return askDeploy(branchName, PKG, argv);
-
-    // return logCommits(branchName, flowType, argv.website).then((data) => {
-    //     const name = PKG.name.toLowerCase().replace(/^(proton|protonmail)-/, '');
-    //     const project = name === 'web' ? 'webclient' : name;
-    //     const json = {
-    //         project,
-    //         "config": {
-    //             "prod": "c0cdec5"
-    //         }
-    //     }
-    //     console.log(data)
-
-    // });
 }
 
 if (argv._.includes('changelog')) {
